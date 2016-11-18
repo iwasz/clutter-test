f6e40f3d9c763fcf4cfb23aea8c4c9343fdb1767
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2016-11-18 11:39:50 +0100

    CbButton from cook-book added

--- src/main.cc
+++ src/main.cc
@@ -8,9 +8,16 @@
 
 #include <clutter/clutter.h>
 #include <iostream>
+#include "cb_button.h"
+
+/* colors */
+static const ClutterColor stage_color = { 0x33, 0x33, 0x55, 0xff };
+static const ClutterColor white_color = { 0xff, 0xff, 0xff, 0xff };
+static const ClutterColor yellow_color = { 0x88, 0x88, 0x00, 0xff };
 
 void on_stage_button_press (ClutterStage *stage, ClutterEvent *event, gpointer data);
 void clicked_cb (ClutterClickAction *action, ClutterActor *actor, gpointer user_data);
+void on_paint (ClutterActor *actor, gpointer user_data);
 
 /*****************************************************************************/
 
@@ -29,25 +36,58 @@ int main (int argc, char **argv)
 
         /*---------------------------------------------------------------------------*/
 
-        ClutterColor actor_color = { 0, 255, 0, 128 };
-        ClutterActor *rect = clutter_actor_new ();
-        clutter_actor_set_background_color (rect, &actor_color);
-        clutter_actor_set_size (rect, 100, 100);
-        clutter_actor_set_position (rect, 100, 100);
-        clutter_actor_add_child (CLUTTER_ACTOR (stage), rect);
-        clutter_actor_set_reactive (rect, TRUE);
+        {
+                ClutterColor actor_color = { 0, 255, 0, 128 };
+                ClutterActor *rect = clutter_actor_new ();
+                clutter_actor_set_background_color (rect, &actor_color);
+                clutter_actor_set_size (rect, 100, 100);
+                clutter_actor_set_position (rect, 100, 100);
+                clutter_actor_add_child (CLUTTER_ACTOR (stage), rect);
+                clutter_actor_set_reactive (rect, TRUE);
 
-//        ClutterAction *clickAction = clutter_click_action_new ();
-//        clutter_actor_add_action (rect, clickAction);
-//        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
+                //        ClutterAction *clickAction = clutter_click_action_new ();
+                //        clutter_actor_add_action (rect, clickAction);
+                //        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
 
-        ClutterAction *dragAction = clutter_drag_action_new ();
-        clutter_actor_add_action (rect, dragAction);
-        //        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
+                ClutterAction *dragAction = clutter_drag_action_new ();
+                clutter_actor_add_action (rect, dragAction);
+                //        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
 
-        clutter_actor_show (rect);
 
-        //        g_signal_connect (stage, "button-press-event", G_CALLBACK (on_stage_button_press), NULL);
+                //        g_signal_connect (stage, "button-press-event", G_CALLBACK (on_stage_button_press), NULL);
+        }
+
+        /*---------------------------------------------------------------------------*/
+
+        {
+                ClutterColor actor_color = { 0, 0, 255, 128 };
+                ClutterActor *rect = clutter_actor_new ();
+                clutter_actor_set_background_color (rect, &actor_color);
+                clutter_actor_set_size (rect, 100, 100);
+                clutter_actor_set_position (rect, 300, 300);
+                clutter_actor_add_child (CLUTTER_ACTOR (stage), rect);
+                clutter_actor_set_reactive (rect, TRUE);
+
+                //        ClutterAction *clickAction = clutter_click_action_new ();
+                //        clutter_actor_add_action (rect, clickAction);
+                //        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
+
+                ClutterAction *dragAction = clutter_drag_action_new ();
+                clutter_actor_add_action (rect, dragAction);
+                //        g_signal_connect (clickAction, "clicked", G_CALLBACK (clicked_cb), NULL);
+
+//                clutter_actor_show (rect);
+                //        g_signal_connect (stage, "button-press-event", G_CALLBACK (on_stage_button_press), NULL);
+        }
+
+        /*---------------------------------------------------------------------------*/
+
+        ClutterActor *button = cb_button_new ();
+        cb_button_set_text (CB_BUTTON (button), "hello");
+        /* note that the size of the button is left to Clutter's size requisition */
+        cb_button_set_text_color (CB_BUTTON (button), &white_color);
+        cb_button_set_background_color (CB_BUTTON (button), &yellow_color);
+        clutter_actor_add_child (stage, button);
 
         /*---------------------------------------------------------------------------*/
 
@@ -81,4 +121,38 @@ void clicked_cb (ClutterClickAction *action, ClutterActor *actor, gpointer user_
          * to the number returned by clutter_event_get_button())
          */
         std::cerr << "Button " << clutter_click_action_get_button (action) << " clicked" << std::endl;
+}
+
+void on_paint (ClutterActor *actor, gpointer user_data)
+{
+        ClutterActorBox allocation = {
+                0,
+        };
+        gfloat width, height;
+        guint tmp_alpha;
+
+        /* priv is a private internal struct */
+        //        ClutterColor color = STAR_ACTOR (actor)->priv->color;
+
+        clutter_actor_get_allocation_box (actor, &allocation);
+        clutter_actor_box_get_size (&allocation, &width, &height);
+
+        //        tmp_alpha = clutter_actor_get_paint_opacity (actor) * color.alpha / 255;
+
+        cogl_path_new ();
+
+        //        cogl_set_source_color4ub (color.red, color.green, color.blue, tmp_alpha);
+        cogl_set_source_color4ub (0, 0, 255, 128);
+
+        /* create and store a path describing a star */
+        cogl_path_move_to (width * 0.5, 0);
+        cogl_path_line_to (width, height * 0.75);
+        cogl_path_line_to (0, height * 0.75);
+        cogl_path_move_to (width * 0.5, height);
+        cogl_path_line_to (0, height * 0.25);
+        cogl_path_line_to (width, height * 0.25);
+        cogl_path_line_to (width * 0.5, height);
+
+        cogl_path_fill ();
+        g_signal_stop_emission_by_name (actor, "paint");
 }
